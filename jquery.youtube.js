f5951afd5089c0d29b8bd2a8bfaa44fcbc88c574
Tiago Andrade <tiago@imagexmedia.com>
2015-05-14 23:17:15 -0700

    Google API V3

--- jquery.youtube.js
+++ jquery.youtube.js
@@ -1,130 +1,136 @@
 /*
  * YouTube Videos Pugin - for jQuery 1.3+
  *
- * Copyright 2011, Tiago Ayer de Andrade
+ * Copyright 2015, Tiago Ayer de Andrade
  * http://www.woombo.com/
  *
- * Version: 1.0 (Oct 7th 2011)
+ * Version: APIV2.0 (Oct 7th 2011)
+ *          APIV3.0 (May 14th 2015)
  */
- 
+
 (function($){
   $.fn.youtube = function(settings) {
 
-    var options = { 
-      results: '1',
-      user: '',
-      width: '560', 
-      height: '315', 
-      type: 'list', 
-      imageSize: 'thumb', 
-      topPlayer: false, 
-      jsonURL: 'http://gdata.youtube.com/feeds/api/users/#USERNAME#/uploads?v=2&alt=json-in-script&start-index=1&max-results=#RESULTS#', 
+    var options = {
+      channelId: '',
+      key: '',
+      maxResults: '1',
+      width: '560',
+      height: '315',
+      type: 'list',
+      imageSize: 'thumb',
+      topPlayer: false,
+      jsonURL: 'https://www.googleapis.com/youtube/v3/search?channelId=:channelId&key=:key&part=snippet&maxResults=:maxResults',
       ellipsisText: 'loading videos...'
     };
 
-    if( options){
-      $.extend( options, settings);
+    if(options){
+      $.extend(options, settings);
     }
-  
+
     return this.each(function(){
-    
+
       // SET VARS
-      xUsername     = options.user;
-      xResults      = options.results < 1 ? 1 : options.results;
-      xURL			= options.jsonURL;
-      xWidth		= options.width;
-      xHeight		= options.height;
-      xType			= options.type;
-      xTopPlayer	= options.topPlayer;
+      xChannelId  = options.channelId,
+      xKey        = options.key,
+      xMaxResults = options.maxResults < 1 ? 1 : options.maxResults;
+      xURL        = options.jsonURL;
+      xWidth      = options.width;
+      xHeight     = options.height;
+      xType       = options.type;
+      xTopPlayer  = options.topPlayer;
       xEllipsisText = options.ellipsisText;
-    
+
       // CHECK USERNAME
-      if( !xUsername){ 
+      if(!xChannelId || !xKey){
+        console.log('channelId and key are mandatory.');
         return false;
       }
-    
+
       // LOAD OBJECT
       obj = $(this);
 
       // APPEND CONTAINER
-      classContainerLoading	= 'youtube-videos-loading'; 
-      classContainerVideos	= 'youtube-videos-container';
-      classContainerPlayer	= 'youtube-videos-player';
-			
+      classContainerLoading = 'youtube-videos-loading';
+      classContainerVideos  = 'youtube-videos-container';
+      classContainerPlayer  = 'youtube-videos-player';
+
       obj.append('<div class="youtube-videos-wraper">'
-        + '	<div class="'+ classContainerVideos +'">'
-        + '		<div class="'+ classContainerPlayer +'"></div>'
-        + '		<div class="'+ classContainerLoading +'">'+ xEllipsisText +'</div>'
+        + ' <div class="'+ classContainerVideos +'">'
+        + '   <div class="'+ classContainerPlayer +'"></div>'
+        + '   <div class="'+ classContainerLoading +'">'+ xEllipsisText +'</div>'
         + ' </div>'
         + '</div>');
 
       // REPLACE STRINGS
-      setUsername	= xURL.replace( '#USERNAME#', xUsername);
-      setResults	= setUsername.replace( '#RESULTS#', xResults);
-      finalURL 		= setResults;
+      finalURL = xURL
+        .replace(':channelId', xChannelId)
+        .replace(':key', xKey)
+        .replace(':maxResults', xMaxResults);
 
-      function buildIFrame( hash){
+      function buildIFrame(hash){
         return '<iframe width="'+ xWidth +'" height="'+ xHeight +'" src="http://www.youtube.com/embed/'+ hash +'?wmode=transparent" frameborder="0" allowfullscreen></iframe>';
       }
-			
-      function setSelectedItem( itemID){ 
+
+      function setSelectedItem(itemID){
         // DESELECT ALL
         obj.find('.youtube-list-item').removeClass('selected');
-				
+
         // SELECT ITEM
-        obj.find('#'+itemID).addClass('selected');
+        obj.find('#'+ itemID).addClass('selected');
       }
 
       // LOAD JSON
       $.ajax({
-        type: 'GET',
         url: finalURL,
-        cache: false,
-        dataType:'jsonp',
-        success: function(data){ 
-			  
-          items = data.feed.entry;
-			  	
-          if( !items){
+        dataType: 'json',
+        success: function(data){
+
+          items = data.items;
+
+          if(!items){
             return false;
           }
-			  	
+
           var holdData = '';
 
-          $.each( items, function( i, item) {
+          $.each(items, function(i, item) {
 
-            info  = item.media$group;
-            ID    = info.yt$videoid.$t;
-            title = info.media$title.$t;
-            thumb = info.media$thumbnail[0].url;
-            image = info.media$thumbnail[1].url; 
 
-            setClass	= 'youtube-list-item youtube-item-row-'+ i;
-            //setIframe = '<div class="youtube-item-embed"><iframe width="'+ xWidth +'" height="'+ xHeight +'" src="http://www.youtube.com/embed/'+ ID +'" frameborder="0" allowfullscreen></iframe></div>';
-            setIframe   = '<div class="youtube-item-embed">'+ buildIFrame(ID) +'</div>';
-            setLink		= 'http://www.youtube.com/watch?v='+ ID;
-            setID		= 'youtube-item-'+ ID;
-            setImage	= options.imageSize == 'thumb' ? thumb : image;
-						
-            if( i == 0){ 
-              firstItemHash = ID;
+            var videoId = item.id.videoId,
+                publishedAt = item.snippet.publishedAt,
+                title = item.snippet.title,
+                thumbnail = {
+                  default: item.snippet.thumbnails.default.url,
+                  high: item.snippet.thumbnails.high.url,
+                  medium: item.snippet.thumbnails.medium.url
+                };
+
+            setClass  = 'youtube-list-item youtube-item-row-'+ i;
+            setIframe   = '<div class="youtube-item-embed">'+ buildIFrame(videoId) +'</div>';
+            setLink   = 'http://www.youtube.com/watch?v='+ videoId;
+            setID   = 'youtube-item-'+ videoId;
+            setImage  = options.imageSize == 'thumb' ? thumbnail.medium : thumbnail.high;
+
+            if(i == 0){
+              firstItemHash = videoId;
             }
-						
-            if( xType == 'image-list'){ 
+
+            if(xType == 'list'){
               loadedItem = '<div id="'+ setID +'" class="'+ setClass +'">'
-              + '	<div class="youtube-item-image">'
-              + '		<a href="'+ setLink +'" title="'+ title +'" target="_blank">'
-              + '			<img src="'+ setImage +'" alt="'+ title +'" />'
-              + '		</a>'
-              + '	</div>'
-              + '	<div class="youtube-item-title">'
-              + '		<strong>'+ title +'</strong>'
-              + '	</div>'
+              + ' <div class="youtube-item-image">'
+              + '   <a href="'+ setLink +'" title="'+ title +'" target="_blank">'
+              + '     <img src="'+ setImage +'" alt="'+ title +'" />'
+              + '   </a>'
+              + ' </div>'
+              + ' <div class="youtube-item-title">'
+              + '   <strong>'+ title +'</strong>'
+              + ' </div>'
               + '</div>';
             }
-            else{ 
+            else{
               loadedItem = '<div id="'+ setID +'" class="'+ setClass +'">'
-              + '	<strong class="youtube-item-title">'+ title +'</strong>'
+              + ' <strong class="youtube-item-title">'+ title +'</strong>'
               + setIframe
               + '</div>';
             }
@@ -133,24 +139,24 @@
 
           });
 
-          obj.find('div.'+ classContainerLoading).fadeOut('slow', function(){ 
-					
+          obj.find('div.'+ classContainerLoading).fadeOut('slow', function(){
+
             // APPEND DATA
             obj.find('div.'+ classContainerVideos).hide().append(holdData).fadeIn('slow');
-						
+
             // TOP PLAYER
-            if( xTopPlayer == true){ 
-						
+            if(xTopPlayer == true){
+
               // ADD PLAYER
-              obj.find('div.'+ classContainerPlayer).html( buildIFrame( firstItemHash));
-							
+              obj.find('div.'+ classContainerPlayer).html(buildIFrame(firstItemHash));
+
               //BIND CLICK
-              obj.find('a').click( function(e){ 
+              obj.find('a').click(function(e){
                 e.preventDefault();
-                loadHash = $(this).attr('href').replace( 'http://www.youtube.com/watch?v=', '');
+                loadHash = $(this).attr('href').replace('http://www.youtube.com/watch?v=', '');
                 loadID = $(this).parents('.youtube-list-item').attr('id');
                 setSelectedItem(loadID);
-                obj.find('div.'+ classContainerPlayer).html( buildIFrame( loadHash));
+                obj.find('div.'+ classContainerPlayer).html(buildIFrame(loadHash));
               });
             }
 
